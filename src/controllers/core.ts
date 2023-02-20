bf717c6809912d36f7858e5e1e3d65ede48560f3
alexcos20 <alex.coseru@gmail.com>
2023-02-20 17:20:46 +0200

    add uniqueConsumers

--- src/controllers/core.ts
+++ src/controllers/core.ts
@@ -6,6 +6,68 @@ import {
 import { networks } from '../config/index'
 import fetch from 'cross-fetch'
 
+// unique consumers per week
+async function getUniqueConsumersPerWeek(
+  chainId: number,
+  from: number,
+  to: number
+) {
+  const subgraphUrl = getSubgraphUrlFromChainId(chainId)
+  let skip = 0
+  const nfts = {}
+  const weeks = getWeeksOfYear(from, to)
+  for (const week of weeks) nfts[week] = {}
+  do {
+    const query = {
+      query: `query{
+                  orders(where:{createdTimestamp_gte:${from} createdTimestamp_lt:${to}} skip:${skip}, first:1000 orderBy:createdTimestamp orderDirection:asc){
+                    createdTimestamp
+                    consumer {id}
+                  }
+              }`
+    }
+    //console.log(query)
+    const response = await fetch(subgraphUrl, {
+      method: 'POST',
+      body: JSON.stringify(query)
+    })
+    const respJSON = await response.json()
+    skip = skip + 1000
+    if (!respJSON.data || respJSON.data.orders.length < 1) {
+      break
+    }
+    for (const row of respJSON.data.orders) {
+      const key = getYearAndWeek(row.createdTimestamp)
+      nfts[key][row.consumer.id] = true
+    }
+    // eslint-disable-next-line no-constant-condition
+  } while (true)
+  return nfts
+}
+
+export async function getUniqueConsumers(from: number, to: number) {
+  const prom = []
+  const results = {}
+  const weeks = getWeeksOfYear(from, to)
+  for (const week of weeks) results[week] = 0
+  let i = 0
+  for (const network of networks) {
+    prom[i] = getUniqueConsumersPerWeek(network.chainId, from, to)
+    i++
+  }
+  const allPromises = await Promise.all(prom)
+  for (const week of weeks) {
+    i = 0
+    for (const network of networks) {
+      results[week] += Object.keys(allPromises[i][week]).length
+      i++
+    }
+  }
+
+  return results
+}
+
+
 async function getNoOfErc721PerChain(
   chainId: number,
   from: number,
--- src/routes/core.ts
+++ src/routes/core.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 //import accessController from '../controllers/accessController'
 import express from 'express'
-import { getNoOfErc721 } from '../controllers/core'
+import { getNoOfErc721, getUniqueConsumers } from '../controllers/core'
 
 const router = express.Router()
 
@@ -14,6 +14,17 @@ router.get('/publishedNFT', async function (req: Request, res: Response) {
   if (!to) limit = now
   else limit = parseInt(to as string)
   const deposits = await getNoOfErc721(from, limit)
+  res.status(200).send(deposits)
+})
+router.get('/uniqueConsumers', async function (req: Request, res: Response) {
+  const { since, to } = req.query
+  let from, limit
+  const now = Math.floor(Date.now() / 1000)
+  if (!since) from = now - 15780000 // 6 months
+  else from = parseInt(since as string)
+  if (!to) limit = now
+  else limit = parseInt(to as string)
+  const deposits = await getUniqueConsumers(from, limit)
   res.status(200).send(deposits)
 })
 
