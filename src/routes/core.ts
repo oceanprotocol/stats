a651aed96cdb0b46bc25e6fadf0f167b60466f14
alexcos20 <alex.coseru@gmail.com>
2023-03-06 11:09:26 +0200

    add cache

--- src/routes/core.ts
+++ src/routes/core.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express'
 //import accessController from '../controllers/accessController'
 import express from 'express'
+import cacheService from 'express-api-cache'
+const cache = cacheService.cache
 import {
   getNoOfErc721,
   getUniqueConsumers,
@@ -9,30 +11,39 @@ import {
 
 const router = express.Router()
 
-router.get('/publishedNFT', async function (req: Request, res: Response) {
-  const { since, to } = req.query
-  let from, limit
-  const now = Math.floor(Date.now() / 1000)
-  if (!since) from = now - 15780000 // 6 months
-  else from = parseInt(since as string)
-  if (!to) limit = now
-  else limit = parseInt(to as string)
-  const deposits = await getNoOfErc721(from, limit)
-  res.status(200).send(deposits)
-})
-router.get('/uniqueConsumers', async function (req: Request, res: Response) {
-  const { since, to } = req.query
-  let from, limit
-  const now = Math.floor(Date.now() / 1000)
-  if (!since) from = now - 15780000 // 6 months
-  else from = parseInt(since as string)
-  if (!to) limit = now
-  else limit = parseInt(to as string)
-  const deposits = await getUniqueConsumers(from, limit)
-  res.status(200).send(deposits)
-})
+router.get(
+  '/publishedNFT',
+  cache('10 minutes'),
+  async function (req: Request, res: Response) {
+    const { since, to } = req.query
+    let from, limit
+    const now = Math.floor(Date.now() / 1000)
+    if (!since) from = now - 15780000 // 6 months
+    else from = parseInt(since as string)
+    if (!to) limit = now
+    else limit = parseInt(to as string)
+    const deposits = await getNoOfErc721(from, limit)
+    res.status(200).json(deposits)
+  }
+)
+router.get(
+  '/uniqueConsumers',
+  cache('10 minutes'),
+  async function (req: Request, res: Response) {
+    const { since, to } = req.query
+    let from, limit
+    const now = Math.floor(Date.now() / 1000)
+    if (!since) from = now - 15780000 // 6 months
+    else from = parseInt(since as string)
+    if (!to) limit = now
+    else limit = parseInt(to as string)
+    const deposits = await getUniqueConsumers(from, limit)
+    res.status(200).json(deposits)
+  }
+)
 router.get(
   '/uniquePublishMarkets',
+  cache('10 minutes'),
   async function (req: Request, res: Response) {
     const { since, to } = req.query
     let from, limit
@@ -42,7 +53,7 @@ router.get(
     if (!to) limit = now
     else limit = parseInt(to as string)
     const deposits = await getUniquePublishMarkets(from, limit)
-    res.status(200).send(deposits)
+    res.status(200).json(deposits)
   }
 )
 
--- src/routes/veOceanRoutes.ts
+++ src/routes/veOceanRoutes.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express'
 //import accessController from '../controllers/accessController'
 import express from 'express'
+import cacheService from 'express-api-cache'
+const cache = cacheService.cache
 import {
   veGetLockedAmount,
   veGetDeposits,
@@ -15,27 +17,35 @@ router.get('/lockedAmount', async function (req: Request, res: Response) {
   console.log('lockedAmount:' + lockedAmount)
   res.status(200).send('' + lockedAmount)
 })
-router.get('/deposits', async function (req: Request, res: Response) {
-  const { since, to } = req.query
-  let from, limit
-  const now = Math.floor(Date.now() / 1000)
-  if (!since) from = now - 15780000 // 6 months
-  else from = parseInt(since as string)
-  if (!to) limit = now
-  else limit = parseInt(to as string)
-  const deposits = await veGetDeposits(from, limit)
-  res.status(200).send(deposits)
-})
-router.get('/withdraws', async function (req: Request, res: Response) {
-  const { since, to } = req.query
-  let from, limit
-  const now = Math.floor(Date.now() / 1000)
-  if (!since) from = now - 15780000 // 6 months
-  else from = parseInt(since as string)
-  if (!to) limit = now
-  else limit = parseInt(to as string)
-  const deposits = await veGetWithdraws(from, limit)
-  res.status(200).send(deposits)
-})
+router.get(
+  '/deposits',
+  cache('10 minutes'),
+  async function (req: Request, res: Response) {
+    const { since, to } = req.query
+    let from, limit
+    const now = Math.floor(Date.now() / 1000)
+    if (!since) from = now - 15780000 // 6 months
+    else from = parseInt(since as string)
+    if (!to) limit = now
+    else limit = parseInt(to as string)
+    const deposits = await veGetDeposits(from, limit)
+    res.status(200).json(deposits)
+  }
+)
+router.get(
+  '/withdraws',
+  cache('10 minutes'),
+  async function (req: Request, res: Response) {
+    const { since, to } = req.query
+    let from, limit
+    const now = Math.floor(Date.now() / 1000)
+    if (!since) from = now - 15780000 // 6 months
+    else from = parseInt(since as string)
+    if (!to) limit = now
+    else limit = parseInt(to as string)
+    const deposits = await veGetWithdraws(from, limit)
+    res.status(200).json(deposits)
+  }
+)
 
 export default router
