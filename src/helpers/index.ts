26c84b212ca4fb773dce3b2f2fe82edab1998974
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2023-04-13 19:19:57 +0300

    added totalUniquePublishMarkets yearlyUniquePublishMarkets montlyUniquePublishMarkets

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -22,6 +22,23 @@ export function getYearAndWeek(timestamp: number) {
   const key = tdt.getFullYear() + '-' + week
   return key
 }
+
+export function getYearAndMonth(timestamp: number) {
+  const dt = new Date(timestamp * 1000)
+  const year = dt.getFullYear()
+  const month = dt.getMonth() + 1
+  const monthStr = month < 10 ? '0' + month : month.toString()
+  const key = `${year}-${monthStr}`
+  return key
+}
+
+export function getYear(timestamp: number) {
+  const dt = new Date(timestamp * 1000)
+  const year = dt.getFullYear()
+  const key = `${year}`
+  return key
+}
+
 export function getWeeksOfYear(from: number, to: number) {
   // returns all weeks from to
   const keys = []
@@ -31,5 +48,44 @@ export function getWeeksOfYear(from: number, to: number) {
     keys.push(getYearAndWeek(current))
     current += 7 * 86400
   } while (current < to)
+  return keys
+}
+
+function addOneMonth(timestamp) {
+  const date = new Date(timestamp * 1000)
+  date.setMonth(date.getMonth() + 1)
+  return Math.floor(date.getTime() / 1000)
+}
+
+export function getMontsOfYear(from: number, to: number) {
+  // returns all months from to
+  const keys = []
+
+  let current = from
+  do {
+    keys.push(getYearAndMonth(current))
+    current = addOneMonth(current)
+  } while (current < to)
+  return keys
+}
+
+function addOneYear(timestamp) {
+  const date = new Date(timestamp * 1000)
+  date.setFullYear(date.getFullYear() + 1)
+  return Math.floor(date.getTime() / 1000)
+}
+
+export function getYears(from: number, to: number) {
+  // returns all months from to
+  const keys = []
+  let current = from
+  let currentDate = new Date(from * 1000)
+  const endDate = new Date(to * 1000)
+
+  do {
+    keys.push(getYear(current))
+    current = addOneYear(current)
+    currentDate = new Date(current * 1000)
+  } while (currentDate.getFullYear() <= endDate.getFullYear())
   return keys
 }
