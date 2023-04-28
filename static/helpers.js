328cc9ed4bcb8e3c20b28017aef5c4e39628d661
alexcos20 <alex.coseru@gmail.com>
2023-04-28 16:33:50 +0300

    fix typo

--- static/helpers.js
+++ static/helpers.js
@@ -1,7 +1,7 @@
 
 function clearChart(chart,title){
     chart.data.labels=[]
-    for(i=0;i<chart.data.datasets;i++)
+    for(i=0;i<chart.data.datasets.length;i++)
 			chart.data.datasets[i].data=[]
 	chart.options.title.text=title
 }
