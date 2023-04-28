57a5c2ae2189c2a600ab55feaf1ae94e0708e217
alexcos20 <alex.coseru@gmail.com>
2023-04-28 17:11:51 +0300

    add free/paid consumes

--- src/routes/core.ts
+++ src/routes/core.ts
@@ -9,6 +9,7 @@ import {
   getWeeklyUniquePublishMarkets,
   getMontlyUniquePublishMarkets,
   getFreeOrders,
+  getPaidOrders,
   getOceanOrders,
   getUniqueConsumeMarkets,
   getYearlyUniquePublishMarkets,
@@ -142,6 +143,21 @@ router.get(
     if (!to) limit = now
     else limit = parseInt(to as string)
     const deposits = await getFreeOrders(from, limit)
+    res.status(200).json(deposits)
+  }
+)
+router.get(
+  '/paidConsumes',
+  cache('10 minutes'),
+  async function (req: Request, res: Response) {
+    const { since, to } = req.query
+    let from, limit
+    const now = Math.floor(Date.now() / 1000)
+    if (!since) from = now - 15780000 // 6 months
+    else from = parseInt(since as string)
+    if (!to) limit = now
+    else limit = parseInt(to as string)
+    const deposits = await getPaidOrders(from, limit)
     res.status(200).json(deposits)
   }
 )
